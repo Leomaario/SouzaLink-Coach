299d12aa718f68bd10f75121a75bdd326963162e
Leomaario <leomatiosaouza11@gmail.com>
2025-07-28 16:39:33 -0300

    teste de codigo para bug de stream morto

--- vite.config.js
+++ vite.config.js
@@ -6,27 +6,30 @@ import { fileURLToPath } from 'url';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, 
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', 
-        changeOrigin: true, 
-        secure: false, 
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
       },
       '/api/auth': {
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
-      
     },
   },
-  resolve:{
+  resolve: {
     alias: {
       '@styles': Path.resolve(__dirname, './src/Styles'),
       '@components': Path.resolve(__dirname, './src/Components'),
       '@pages': Path.resolve(__dirname, './src/Pages')
     }
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-player']
+    }
   }
-  
 });
